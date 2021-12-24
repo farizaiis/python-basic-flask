42a555cb302d1a22da9a2171bd0e3413fab0c9b7
Adi Firman Ilham <firmanilham19@gmail.com>
2021-12-24 21:15:59 +0700

    add some method for string

---
+++ String/material_23.py
@@ -0,0 +1,11 @@
+# using enumerate()
+mystr = 'university'
+
+my_list_enumerate = list(enumerate(mystr))
+print('list(enumerate(mystr)) ', my_list_enumerate)
+# Output = 
+
+# use \n to a new line. example = 'ABC\nDEF'
+# use \t to a new line. example = 'ABC\tDEF'
+print('ABC written \x41\x42\x43\x44 (HEX) Presentation')
+# Output = 
