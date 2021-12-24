218fc3f79ff79e7ada89008dd0641d3e13ad5b55
Adi Firman Ilham <firmanilham19@gmail.com>
2021-12-24 20:26:15 +0700

    add naming module example

---
+++ Numpy/material_18.py
@@ -0,0 +1,11 @@
+import math as m
+print('The value of pi is', m.pi)
+# Output = The value of pi is 3.141592653589793
+
+from math import pi
+print('The value of pi is', pi)
+# Output = The value of pi is 3.141592653589793
+
+from math import *
+print('The value of pi is', pi)
+# Output = The value of pi is 3.141592653589793
